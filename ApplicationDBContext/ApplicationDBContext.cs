818ba08057510ab7655459b474d1f61419fadd37
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-03-23 04:41:04 +0400

    Changes to DbContext for DataMigration

--- ApplicationDBContext/ApplicationDBContext.cs
+++ ApplicationDBContext/ApplicationDBContext.cs
@@ -23,20 +23,20 @@ public class ApplicationDBContext : DbContext
         modelBuilder.Entity<Person>().Property(p => p.LastName).HasColumnType("Nvarchar(50)").IsRequired().HasAnnotation("MinLength", 2);
         modelBuilder.Entity<Person>().Property(p => p.PersonalNumber).HasColumnType("Nvarchar(11)").IsRequired();
         modelBuilder.Entity<Person>().Property(p => p.DateOfBirth).IsRequired().HasColumnType("Date").HasAnnotation("CheckConstraint", "DateOfBirth <= DateAdd(Year, -18, GetDate())");
-        //Picture
+        /* Picture */
         modelBuilder.Entity<Person>().HasMany(c => c.PhoneNumbers).WithOne(c => c.PersonId);
-        modelBuilder.Entity<Person>().Property(p => p.CreateDate).IsRequired().HasColumnType("Date").HasDefaultValue("GetDate()");
-        modelBuilder.Entity<Person>().Property(p => p.IsActive).IsRequired().HasColumnType("Bit").HasDefaultValue("(0)");
+        modelBuilder.Entity<Person>().Property(p => p.CreateDate).IsRequired().HasColumnType("Date").HasDefaultValueSql("GetDate()");
+        modelBuilder.Entity<Person>().Property(p => p.IsActive).IsRequired().HasColumnType("Bit").HasDefaultValue(false);
 
         modelBuilder.Entity<Relationship>().Property(r => r.RelationshipType).IsRequired();
-        modelBuilder.Entity<Relationship>().HasKey(r => new { r.FromPerson, r.ToPerson });
-        modelBuilder.Entity<Relationship>().HasOne(r => r.FromPerson).WithMany(p => p.RelationshipFrom).HasForeignKey(r => r.FromPersonId);
-        modelBuilder.Entity<Relationship>().HasOne(r => r.ToPerson).WithMany(p => p.RelationshipTo).HasForeignKey(r => r.ToPersonId);
+        modelBuilder.Entity<Relationship>().HasKey(r => new { r.FromPersonId, r.ToPersonId });
+        modelBuilder.Entity<Relationship>().HasOne(r => r.FromPerson).WithMany(p => p.RelationshipFrom).HasForeignKey(r => r.FromPersonId).OnDelete(DeleteBehavior.NoAction);
+        modelBuilder.Entity<Relationship>().HasOne(r => r.ToPerson).WithMany(p => p.RelationshipTo).HasForeignKey(r => r.ToPersonId).OnDelete(DeleteBehavior.NoAction);
 
         modelBuilder.Entity<PhoneNumber>().Property(p => p.Number).HasColumnType("Nvarchar(50)").IsRequired().HasAnnotation("MinLength", 4);
-        modelBuilder.Entity<PhoneNumber>().Property(p => p.IsActive).HasColumnType("bit").HasDefaultValueSql("(0)");
+        modelBuilder.Entity<PhoneNumber>().Property(p => p.IsActive).HasColumnType("bit").HasDefaultValue(false);
         modelBuilder.Entity<PhoneNumber>().Property(p => p.CreateDate).HasColumnType("date").HasDefaultValueSql("GetDate()");
-        modelBuilder.Entity<PhoneNumber>().HasOne(p => p.PersonId).WithMany(p => p.PhoneNumbers).HasForeignKey(p => p.PersonId);
+        modelBuilder.Entity<PhoneNumber>().HasOne(p => p.PersonId).WithMany(p => p.PhoneNumbers);
 
     }
 
