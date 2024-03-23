5e6b06c047a071b2fdc23f2d3e215567fff1c411
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-03-23 05:28:06 +0400

    Uploading migration ( .cs , .Designer.cs , ModelSnapshot.cs ) Problem with creating tables in SSMS

---
+++ ApplicationDBContext/Migrations/20240323011136_Initial.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "date", nullable: false, defaultValueSql: "GetDate()"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "Nvarchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "Nvarchar(50)", nullable: false),
+                    Gender = table.Column<byte>(type: "tinyint", nullable: false),
+                    PersonalNumber = table.Column<string>(type: "Nvarchar(11)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "Date", nullable: false),
+                    CityId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "Date", nullable: false, defaultValueSql: "GetDate()"),
+                    IsActive = table.Column<bool>(type: "Bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneNumbers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonIdId = table.Column<int>(type: "int", nullable: false),
+                    PhoneType = table.Column<byte>(type: "tinyint", nullable: false),
+                    Number = table.Column<string>(type: "Nvarchar(50)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "date", nullable: false, defaultValueSql: "GetDate()"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneNumbers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PhoneNumbers_Persons_PersonIdId",
+                        column: x => x.PersonIdId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Relationships",
+                columns: table => new
+                {
+                    FromPersonId = table.Column<int>(type: "int", nullable: false),
+                    ToPersonId = table.Column<int>(type: "int", nullable: false),
+                    RelationshipType = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relationships", x => new { x.FromPersonId, x.ToPersonId });
+                    table.ForeignKey(
+                        name: "FK_Relationships_Persons_FromPersonId",
+                        column: x => x.FromPersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Relationships_Persons_ToPersonId",
+                        column: x => x.ToPersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name",
+                table: "Cities",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_CityId",
+                table: "Persons",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhoneNumbers_PersonIdId",
+                table: "PhoneNumbers",
+                column: "PersonIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_ToPersonId",
+                table: "Relationships",
+                column: "ToPersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhoneNumbers");
+
+            migrationBuilder.DropTable(
+                name: "Relationships");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
