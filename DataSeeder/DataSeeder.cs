54fc4a956858466dc6fc9a45af27b063f8726d98
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-03-23 05:58:50 +0400

    DataSeeder experimentation

---
+++ DataSeeder/DataSeeder.cs
@@ -0,0 +1,27 @@
+﻿using Task.Repository;
+using Task.DTO;
+
+namespace Seeder;
+
+internal class DataSeeder
+{
+    private readonly ApplicationDBContext _DbContext;
+
+    public DataSeeder(ApplicationDBContext dbContext)
+    {
+        _DbContext = dbContext;
+    }
+
+    public void SeedData()
+    {
+        if (!_DbContext.Cities.Any())
+        {
+            _DbContext.Cities.AddRange(
+                new City { Id = 1, Name = "Tbilisi" },
+                new City { Id = 2, Name = "Batumi" }
+            );
+
+            _DbContext.SaveChanges();
+        }
+    }
+}
