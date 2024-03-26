7b261ed18be79e8ce2b9dfbf466c64e2087dfb96
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-03-26 18:00:22 +0400

    minor changes to fluent api

--- Configuration/PersonConfiguration.cs
+++ Configuration/PersonConfiguration.cs
@@ -7,15 +7,50 @@ public class PersonConfiguration : IEntityConfiguration
 {
     public bool Configure(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Person>().HasKey(p => p.Id);
-        modelBuilder.Entity<Person>().Property(p => p.FirstName).IsRequired().HasColumnType("Nvarchar(50)").HasAnnotation("MinLength", 2);
-        modelBuilder.Entity<Person>().Property(p => p.LastName).IsRequired().HasColumnType("Nvarchar(50)").HasAnnotation("MinLength", 2);
-        modelBuilder.Entity<Person>().Property(p => p.PersonalNumber).IsRequired().HasColumnType("Nvarchar(11)").HasAnnotation("MinLength", 11);
-        modelBuilder.Entity<Person>().Property(p => p.DateOfBirth).IsRequired().HasColumnType("Date").HasAnnotation("CheckConstraint", "DateOfBirth <= DateAdd(Year, -18, GetDate())");
+        modelBuilder.Entity<Person>()
+            .HasKey(p => p.Id);
+
+        modelBuilder.Entity<Person>()
+            .Property(p => p.FirstName)
+            .IsRequired()
+            .HasColumnType("Nvarchar(50)")
+            .HasAnnotation("MinLength", 2);
+
+        modelBuilder.Entity<Person>()
+            .Property(p => p.LastName)
+            .IsRequired()
+            .HasColumnType("Nvarchar(50)")
+            .HasAnnotation("MinLength", 2);
+
+        modelBuilder.Entity<Person>()
+            .Property(p => p.PersonalNumber)
+            .IsRequired()
+            .HasColumnType("Nvarchar(11)")
+            .HasAnnotation("MinLength", 11);
+
+        modelBuilder.Entity<Person>()
+            .Property(p => p.DateOfBirth)
+            .IsRequired()
+            .HasColumnType("Date")
+            .HasAnnotation("CheckConstraint", "DateOfBirth <= DateAdd(Year, -18, GetDate())");
+
         /* Picture */
-        modelBuilder.Entity<Person>().HasMany(p => p.PhoneNumbers).WithOne(c => c.Person);
-        modelBuilder.Entity<Person>().Property(p => p.CreateDate).IsRequired().HasColumnType("Date").HasDefaultValueSql("GetDate()");
-        modelBuilder.Entity<Person>().Property(p => p.IsActive).IsRequired().HasColumnType("Bit").HasDefaultValue(false);
+
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.PhoneNumbers)
+            .WithOne(c => c.Person);
+
+        modelBuilder.Entity<Person>()
+            .Property(p => p.CreateDate)
+            .IsRequired()
+            .HasColumnType("Date")
+            .HasDefaultValueSql("GetDate()");
+
+        modelBuilder.Entity<Person>()
+            .Property(p => p.IsActive)
+            .IsRequired()
+            .HasColumnType("Bit")
+            .HasDefaultValue(false);
 
         return true;
     }
--- Configuration/PhoneNumberConfiguration.cs
+++ Configuration/PhoneNumberConfiguration.cs
@@ -7,11 +7,29 @@ public class PhoneNumberConfiguration : IEntityConfiguration
 {
     public bool Configure(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<PhoneNumber>().HasKey(p => p.Id);
-        modelBuilder.Entity<PhoneNumber>().Property(p => p.Number).IsRequired().HasColumnType("Nvarchar(50)").IsRequired().HasAnnotation("MinLength", 4);
-        modelBuilder.Entity<PhoneNumber>().Property(p => p.IsActive).IsRequired().HasColumnType("Bit").HasDefaultValue(false);
-        modelBuilder.Entity<PhoneNumber>().Property(p => p.CreateDate).IsRequired().HasColumnType("Date").HasDefaultValueSql("GetDate()");
-        modelBuilder.Entity<PhoneNumber>().HasOne(p => p.Person).WithMany(p => p.PhoneNumbers);
+        modelBuilder.Entity<PhoneNumber>()
+            .HasKey(p => p.Id);
+
+        modelBuilder.Entity<PhoneNumber>()
+            .Property(p => p.Number)
+            .IsRequired()
+            .HasColumnType("Nvarchar(50)")
+            .HasAnnotation("MinLength", 4);
+
+        modelBuilder.Entity<PhoneNumber>()
+            .Property(p => p.IsActive)
+            .IsRequired()
+            .HasColumnType("Bit")
+            .HasDefaultValue(false);
+
+        modelBuilder.Entity<PhoneNumber>()
+            .Property(p => p.CreateDate)
+            .IsRequired().HasColumnType("Date")
+            .HasDefaultValueSql("GetDate()");
+
+        modelBuilder.Entity<PhoneNumber>()
+            .HasOne(p => p.Person)
+            .WithMany(p => p.PhoneNumbers);
 
         return true;
     }
